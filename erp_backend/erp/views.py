4598687391493f6c78e17df79afa833122281b8b
MahdiChalhoub <chalhoub1995@gmail.com>
2026-02-03 18:09:12 +0000

    [v2.2.1-b593] FIX: Import missing models in saas_stats view

--- erp_backend/erp/views.py
+++ erp_backend/erp/views.py
@@ -9,7 +9,7 @@ from .models import (
     Warehouse, Inventory, InventoryMovement, Unit,
     Brand, Category, Parfum, ProductGroup, Country,
     Contact, Employee, Role, TransactionSequence, BarcodeSettings, Loan, LoanInstallment, FinancialEvent, Transaction, User,
-    OrderLine, JournalEntryLine
+    OrderLine, JournalEntryLine, Module, OrganizationModule
 )
 from .serializers import (
     OrganizationSerializer, SiteSerializer, FinancialAccountSerializer,
