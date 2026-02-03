b2a5323d2fc1d8abad385573651bc0cda30c4a2b
MahdiChalhoub <chalhoub1995@gmail.com>
2026-02-03 03:13:14 +0000

    [v0.2.0-b7] FIX: WarehouseSerializer Attribute Error in get_inventory_count

--- erp_backend/erp/serializers.py
+++ erp_backend/erp/serializers.py
@@ -112,7 +112,7 @@ class WarehouseSerializer(serializers.ModelSerializer):
     inventory_count = serializers.SerializerMethodField()
 
     def get_inventory_count(self, obj):
-        return obj.inventories.count()
+        return obj.inventory.count()
 
     class Meta:
         model = Warehouse
