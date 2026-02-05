b821b3606ffb6e229898c90778bc4cc978486e5c
MahdiChalhoub <chalhoub1995@gmail.com>
2026-02-05 13:50:42 +0000

    [v2.1.0-b005] FIX: Add STATIC_ROOT for production static files

--- erp_backend/core/settings.py
+++ erp_backend/core/settings.py
@@ -167,6 +167,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Production: collectstatic output
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
